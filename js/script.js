e9a56afbbc39e29db3bdf4f18e965cb269ffd49f
GilmarAlves99 <gilmaralves914@gmail.com>
2022-05-25 22:41:47 -0300

    Adicionando Arquivos

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*var i = 0;
+// var A = 0;
+var tag = document.getElementById("hello");
+var html = document.getElementById("hello").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 170;
+
+function typeWriter() {
+    if (i <= txt.length) {
+        document.getElementById("hello").innerHTML = txt.slice(0, i + 1);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    console.log(document.getElementById("text").innerHTML);
+}
+
+typeWriter();
+*/
