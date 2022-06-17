7884707cf7c86dc4582bea352a3b1c2580437fb9
GilmarAlves99 <gilmaralves914@gmail.com>
2022-06-17 14:56:30 -0300

    Termporariamente terminado

--- js/js-scroll.js
+++ js/js-scroll.js
@@ -17,7 +17,8 @@ const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
 function animeScroll() {
-    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    /* const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);*/
+    const windowTop = window.pageYOffset - 100;
     target.forEach(function(element) {
         if ((windowTop) > element.offsetTop) {
             element.classList.add(animationClass);
