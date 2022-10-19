de365b28898e4686100a095c4aef0c5191760778
Gilmar Alves <gilmaralves914@gmail.com>
2022-10-19 13:27:10 -0300

    Atualizando : Portfolio, Curriculos, Slide, Skills

--- js/js-scroll.js
+++ js/js-scroll.js
@@ -18,7 +18,7 @@ const animationClass = 'animate';
 
 function animeScroll() {
     /* const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);*/
-    const windowTop = window.pageYOffset - 100;
+ const windowTop = window.pageYOffset -120;
     target.forEach(function(element) {
         if ((windowTop) > element.offsetTop) {
             element.classList.add(animationClass);
